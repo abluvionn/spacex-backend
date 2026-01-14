9477db3e3b6150a64cb814a84c0360620a6cfae5
abluvionn <sabyraibika@gmail.com>
2026-01-14 19:25:43 +0600

    Add login, refresh token and logout functions

--- routes/auth.js
+++ routes/auth.js
@@ -21,7 +21,7 @@ const generateTokens = (userId) => {
 
 authRouter.post('/register', async (req, res, next) => {
   try {
-    const { email, password, fullName, phone } = req.body;
+    const { email, password, fullName, phone } = req.body || {};
     const userExists = await User.findOne({ email });
     if (userExists) {
       res.status(400).send({ error: 'This email is already taken.' });
@@ -44,6 +44,57 @@ authRouter.post('/register', async (req, res, next) => {
     }
     next(e);
   }
+});
+
+authRouter.post('/login', async (req, res, next) => {
+  try {
+    const { email, password } = req.body || {};
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(401).send({ error: 'Invalid email or password.' });
+      return;
+    }
+    const isPasswordValid = await user.checkPassword(password);
+    if (!isPasswordValid) {
+      res.status(401).send({ error: 'Invalid email or password.' });
+      return;
+    }
+
+    const { accessToken, refreshToken } = generateTokens(user._id);
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      maxAge: config.JwtRefreshExpiresAt * 60 * 60 * 1000, // Convert hours to milliseconds
+    });
+    res.status(200).send({ accessToken, user });
+  } catch (e) {
+    next(e);
+  }
+});
+
+authRouter.post('/refresh-token', (req, res) => {
+  const { refreshToken } = req.cookies || {};
+  if (!refreshToken) {
+    res.status(401).send({ error: 'Refresh token is missing.' });
+    return;
+  }
+  try {
+    const decoded = jwt.verify(refreshToken, process.env.JWT_REFRESH);
+    const { accessToken, refreshToken: newRefreshToken } = generateTokens(
+      decoded.userId
+    );
+    res.cookie('refreshToken', newRefreshToken, {
+      httpOnly: true,
+      maxAge: config.JwtRefreshExpiresAt * 60 * 60 * 1000, // Convert hours to milliseconds
+    });
+    res.status(200).send({ accessToken });
+  } catch (e) {
+    res.status(401).send({ error: 'Invalid refresh token.' });
+  }
+});
+
+authRouter.post('/logout', (_req, res) => {
+  res.clearCookie('refreshToken');
+  res.status(200).send({ message: 'Logged out successfully' });
 });
 
 export default authRouter;
